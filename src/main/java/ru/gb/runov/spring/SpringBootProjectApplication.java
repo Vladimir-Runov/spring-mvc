8f7642a11605a7e96b817b17e73e059576f4c90f
Vladimir-Runov <9322304@gmail.com>
2021-07-14 08:14:20 +0300

    -

--- src/main/java/ru/gb/runov/spring/SpringBootProjectApplication.java
+++ src/main/java/ru/gb/runov/spring/SpringBootProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootProjectApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootProjectApplication.class, args);
 	}
 } 
